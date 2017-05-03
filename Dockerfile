31c3cf72dad686422048e4041d5d1a1b5f1cd51e
Ryan Schuetzler <ryan@schuetzler.net>
2017-05-03 14:09:21 -0500

    Got the Hello Human tutorial working

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ RUN mkdir /code
 WORKDIR /code
 COPY package.json /code
 RUN npm install
-ADD . /code
 
 EXPOSE 3000
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = function(bp) {
   bp.middlewares.load()
+
+  bp.hear(/hello/i, (event, next) => { // We use a regex instead of a hardcoded string
+    const first_name = event.user.first_name
+
+    bp.messenger.sendText(event.user.id, 'Hello, ' + first_name, { typing: true })
+  })
 }
